9f568aaca19b0dd94bd3b02a5dd3fe248b4b194c
Harish Rao <harisvs@gmail.com>
2025-07-06 12:20:09 -0700

    added capacity researvation feature . not tested -missing gile

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "gpu_capacity_block_id" {
+  description = "Capacity block ID for GPU instances. If provided, GPU nodes will use this capacity block."
+  type        = string
+  default     = ""
+} 
